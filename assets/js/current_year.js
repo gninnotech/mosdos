01625a11b6929e82728fb990564ee34b51a57823
gninnotech <141840781+gninnotech@users.noreply.github.com>
2023-08-29 18:32:43 +0800

    Delete assets directory

--- assets/js/current_year.js
+++
@@ -1,5 +0,0 @@
-const today = new Date();
-const currentYear = today.getFullYear();
-
-const yearElement = document.getElementById("id_current_year");
-yearElement.innerHTML = currentYear.toString();

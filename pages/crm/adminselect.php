8bf3f7cf0be733526c75c5cf19b03d81b89e0c0e
gninnotech <141840781+gninnotech@users.noreply.github.com>
2023-08-29 18:33:57 +0800

    Delete pages directory

--- pages/crm/adminselect.php
+++
@@ -1,6 +0,0 @@
-<?php
-        $id_admin = $_SESSION["id_admin"];
-        $adminstmt = $pdo->prepare("SELECT * FROM admin WHERE id_admin = ?");
-        $adminstmt->execute([$id_admin]);
-        $admin = $adminstmt->fetch();
-?>
--- pages/crm/sessiondestroy.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!isset($_SESSION["id_admin"]) && empty($_SESSION["id_admin"])){
-    session_destroy();
-header("Location: ../index.html");
-exit;
-}
-?>
